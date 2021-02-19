49e4070a3404f70113c0e5982a545ab5a2907575
secminhr <secminhrian@gmail.com>
2021-02-19 14:21:19 +0800

    delete confirm alert

--- app/src/main/java/personal/secminhr/cavern/ui/views/articleContent/ArticleContentScreen.kt
+++ app/src/main/java/personal/secminhr/cavern/ui/views/articleContent/ArticleContentScreen.kt
@@ -1,10 +1,8 @@
 package personal.secminhr.cavern.ui.views.articleContent
 
 import android.widget.Toast
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.Edit
@@ -12,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import personal.secminhr.cavern.MainActivity
 import personal.secminhr.cavern.MainActivity.Companion.articleScreen
@@ -45,9 +46,24 @@ class ArticleContentScreen(preview: ArticlePreview): Screen {
         if (showDeleteAlert.value) {
             AlertDialog(
                 onDismissRequest = { showDeleteAlert.value = false },
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
                 title = { Text("Delete?")},
                 text = { Text("Delete this post?\nThere is no way to restore after deletion") },
-                confirmButton = { }
+                confirmButton = {
+                    TextButton(onClick = {
+                        viewModel.deleteArticle(preview.id) {
+                            backToPreviousScreen()
+                        }
+                        showDeleteAlert.value = false
+                    }) {
+                        Text("Delete", color = Color.Red)
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDeleteAlert.value = false }) {
+                        Text("Cancel")
+                    }
+                }
             )
         }
     }
@@ -60,11 +76,8 @@ class ArticleContentScreen(preview: ArticlePreview): Screen {
         Icon(Icons.Default.DeleteForever, "Delete")
     }
 
-    private fun deleteAction(id: Int) {
+    private fun deleteAction() {
         showDeleteAlert.value = true
-        viewModel.deleteArticle(id) {
-            backToPreviousScreen()
-        }
     }
 
     private fun editAction(id: Int) {
@@ -80,7 +93,7 @@ class ArticleContentScreen(preview: ArticlePreview): Screen {
 
     override val topBarIconActions: List<() -> Unit> =
         if (MainActivity.hasCurrentUser && MainActivity.currentAccount!!.username == preview.authorUsername)
-            mutableListOf({ deleteAction(preview.id) }, { editAction(preview.id) }) + super.sameAppBarIconActionAs(articleScreen)
+            mutableListOf(::deleteAction, { editAction(preview.id) }) + super.sameAppBarIconActionAs(articleScreen)
         else
             super.sameAppBarIconActionAs(articleScreen)
 
