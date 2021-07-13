dd7929873d42ce42eaa8eef8d6cc24c375a36332
secminhr <secminhrian@gmail.com>
2021-07-13 16:23:02 +0800

    clean gradle

--- app/src/main/java/personal/secminhr/cavern/ui/views/editor/Editor.kt
+++ app/src/main/java/personal/secminhr/cavern/ui/views/editor/Editor.kt
@@ -1,17 +1,19 @@
 package personal.secminhr.cavern.ui.views.editor
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun Editor(text: State<TextFieldValue>, onTextChange: (TextFieldValue) -> Unit, isError: Boolean) {
 
-    TextField(value = text.value, modifier = Modifier.fillMaxWidth(),
+    TextField(value = text.value, modifier = Modifier.fillMaxWidth().background(Color.White),
         placeholder = { Text("Content") },
         onValueChange = onTextChange,
         isError = isError
