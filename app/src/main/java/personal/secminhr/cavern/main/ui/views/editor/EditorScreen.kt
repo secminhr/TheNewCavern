f4e9669a5c1d7cd5fabc54481ae57f3954c335b8
secminhr <secminhrian@gmail.com>
2021-07-23 18:48:33 +0800

    Merging

--- app/src/main/java/personal/secminhr/cavern/main/ui/views/editor/EditorScreen.kt
+++ app/src/main/java/personal/secminhr/cavern/main/ui/views/editor/EditorScreen.kt
@@ -6,10 +6,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
