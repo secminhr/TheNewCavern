3c5b5837b4193a40bec01669dd6fca42055481ff
secminhr <secminhrian@gmail.com>
2022-01-27 22:08:18 +0800

    LoadingIcon can now fit on different dpi.

--- app/src/main/java/personal/secminhr/cavern/commonUI/LoadingIcon.kt
+++ app/src/main/java/personal/secminhr/cavern/commonUI/LoadingIcon.kt
@@ -1,6 +1,8 @@
 package personal.secminhr.cavern.commonUI
 
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -11,8 +13,11 @@ import androidx.compose.ui.graphics.ClipOp
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import personal.secminhr.cavern.R
 import personal.secminhr.cavern.main.ui.style.logo_green
 import personal.secminhr.cavern.main.ui.style.logo_light_blue
 import personal.secminhr.cavern.main.ui.style.logo_orange
@@ -70,7 +75,7 @@ private fun DrawScope.center() = xHalf() + yHalf()
 
 
 private fun DrawScope.greenCross() {
-    val strokeWidth = 19.7f * scale
+    val strokeWidth = 8.dp.toPx() * scale
     inset(length / 9) {
         for (deg in 0..90 step 90) {
             rotate(deg.toFloat()) {
@@ -87,10 +92,10 @@ private fun DrawScope.greenCross() {
 }
 
 private fun DrawScope.orangeRect(degree: Float) {
-    val strokeWidth = 19.7f * scale
+    val strokeWidth = 8.dp.toPx() * scale
     rotate(degree) {
         //rectSize/length
-        val rectSizeToLengthRatio = 0.12f
+        val rectSizeToLengthRatio = 0.11f
 
         val rectSize = rectSizeToLengthRatio * length
         drawRect(
@@ -110,10 +115,10 @@ private fun DrawScope.blueRect(ratio: Float) {
     val start = center() - xHalf()/2f
     val end = center() - yHalf()/2f
     val outerEnd = lerp(start, end, ratio)
-    val innerYTransition = 78f * scale
+    val innerYTransition = 30.dp.toPx() * scale
     val innerStart = outerEnd + Offset(1f, -1f) * innerYTransition / 2f
 
-    val strokeWidth = 36f * scale
+    val strokeWidth = 15.dp.toPx() * scale
     for (degree in 0..270 step 90) {
         rotate(degree.toFloat()) {
             clipRect(
@@ -151,8 +156,21 @@ fun LoadingIcon_Preview() {
     LoadingIcon()
 }
 
-@Preview(name = "Bigger size 800", widthDp = 800, heightDp = 800, showBackground = true)
+@Preview(name = "dp-sized 435", widthDp = 435, heightDp = 435, showBackground = true)
 @Composable
-fun LoadingIcon_Preview800() {
+fun LoadingIcon_PreviewExprDpSized() {
     LoadingIcon()
 }
+
+@Preview(name = "original img", heightDp = 435, widthDp = 435, showBackground = true)
+@Composable
+fun OriginalImg_Preview() {
+    val painter = painterResource(id = R.drawable.logo_img)
+    Image(painter = painter, contentDescription = "", modifier = Modifier.padding(43.5.dp))
+}
+//
+//@Preview(name = "Height 200", heightDp = 200, showBackground = true)
+//@Composable
+//fun LoadingIcon_PreviewH100() {
+//    LoadingIcon()
+//}
